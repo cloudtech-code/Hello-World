958ae178fc37af3b6267c02cd60174717335f727
taufique2010@gmail.com <taufique2010@gmail.com>
2023-11-17 16:54:09 +0530

    Initial Commit

---
+++ src/main/java/com/cloudtech/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.cloudtech;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello World!!!";
+    }
+
+}
---
+++ src/test/java/com/cloudtech/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
